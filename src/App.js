b85933bf67aea858d57f3db4855d3f3e641b30b6
Jowooseok <whdntjrsla12@naver.com>
2020-10-12 18:11:05 +0900

    app js 수정

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Monitor from "./page/monitor";
 import { BrowserRouter,Route,Switch } from "react-router-dom";
 import Winner from "./page/winner";
 import Rank from "./page/rank";
+import Table from "./page/table";
 const App = () => {
     return (
         <BrowserRouter>
@@ -13,6 +14,7 @@ const App = () => {
                 <Route path="/monitor" component={Monitor}/>
                 <Route path="/winner" component={Winner}/>
                 <Route path="/rank" component={Rank}/>
+                <Route path="/table" component={Table}/>
             </Switch>
 
         </BrowserRouter>
